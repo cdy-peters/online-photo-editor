fa9833a0eeefe9674d233e5c350f6e8c6c9a2d48
Cody Peters <cjosephp15@gmail.com>
2022-08-14 00:49:16 +1000

    Removed EJS

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const express = require("express");
 const app = express();
 const config = require("./config");
 
-app.set("view engine", "ejs");
-
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  res.render("pages/index");
+  res.sendFile("views/index.html", { root: __dirname });
 });
 
 app.listen(config.app.port, () => {
