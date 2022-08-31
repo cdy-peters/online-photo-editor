d9a54962f0971e5a496720631f91f321804cb123
Cody Peters <cjosephp15@gmail.com>
2022-08-31 19:09:33 +1000

    Vercel init

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-require("dotenv");
 const express = require("express");
 const app = express();
-const config = require("./config");
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  res.sendFile("views/index.html", { root: __dirname });
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-app.listen(config.app.port, () => {
-  console.log(`Server is running on port ${config.app.port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
