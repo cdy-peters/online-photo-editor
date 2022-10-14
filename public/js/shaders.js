a320c3352e54b1fa4e4c0032070b9088910ed83b
Cody Peters <cjosephp15@gmail.com>
2022-10-14 14:59:29 +1000

    Beginning of WebGL implementation

---
+++ public/js/shaders.js
@@ -0,0 +1,47 @@
+// Vertex Shader
+const vsSource = `
+  attribute vec2 a_position;
+  attribute vec2 a_texCoord;
+  
+  uniform vec2 u_resolution;
+  
+  varying vec2 v_texCoord;
+  
+  void main() {
+    // convert the rectangle from pixels to 0.0 to 1.0
+    vec2 zeroToOne = a_position / u_resolution;
+
+    // convert from 0->1 to 0->2
+    vec2 zeroToTwo = zeroToOne * 2.0;
+
+    // convert from 0->2 to -1->+1 (clipspace)
+    vec2 clipSpace = zeroToTwo - 1.0;
+
+    gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
+
+    // pass the texCoord to the fragment shader
+    // The GPU will interpolate this value between points.
+    v_texCoord = a_texCoord;
+  }`;
+
+// Fragment Shader
+const fsSource = `
+  precision mediump float;
+  
+  // our texture
+  uniform sampler2D u_image;
+  uniform vec2 u_textureSize;
+  uniform float u_exposure;
+  
+  // the texCoords passed in from the vertex shader.
+  varying vec2 v_texCoord;
+  
+  vec3 adjustExposure(vec3 color, float exposure) {
+    return color * pow(2.0, exposure);
+  }
+  
+  void main() {
+    vec4 color = texture2D(u_image, v_texCoord);
+    color.rgb = adjustExposure(color.rgb, u_exposure);
+    gl_FragColor = color;
+  }`;
---
+++ public/js/webgl.js
@@ -0,0 +1,202 @@
+"use strict";
+
+var capture = false;
+
+const render = (image) => {
+  // Get A WebGL context
+  var canvas = document.querySelector("#canvas");
+  var gl = canvas.getContext("webgl");
+  if (!gl) {
+    return;
+  }
+
+  // Set canvas size
+  canvas.width = image.width;
+  canvas.height = image.height;
+
+  // setup GLSL program
+  const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(vertexShader, vsSource);
+  gl.compileShader(vertexShader);
+
+  const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(fragmentShader, fsSource);
+  gl.compileShader(fragmentShader);
+
+  const program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+
+
+  // look up where the vertex data needs to go.
+  var positionLocation = gl.getAttribLocation(program, "a_position");
+  var texcoordLocation = gl.getAttribLocation(program, "a_texCoord");
+
+  // Create a buffer to put three 2d clip space points in
+  var positionBuffer = gl.createBuffer();
+  // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  // Set a rectangle the same size as the image.
+  setRectangle(gl, 0, 0, image.width, image.height);
+
+  // provide texture coordinates for the rectangle.
+  var texcoordBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([
+      0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0,
+    ]),
+    gl.STATIC_DRAW
+  );
+
+  // Create a texture.
+  var texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  // Set the parameters so we can render any size image.
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+  // Upload the image into the texture.
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+
+  // lookup uniforms
+  var resolutionLocation, textureSizeLocation, exposureLocation;
+  lookupUniforms();
+
+  drawCanvas();
+
+  // Edit image
+  editImage(drawCanvas);
+
+  // Reset image
+  $("#resetButton").on("click", () => {
+    edits = new InitEdits();
+    initValues();
+
+    drawCanvas();
+  });
+
+  // Download image
+  $("#downloadButton").on("click", () => {
+    capture = true;
+    drawCanvas();
+  });
+
+  function drawCanvas() {
+    // Tell WebGL how to convert from clip space to pixels
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    // Clear the canvas
+    gl.clearColor(0, 0, 0, 0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    // Tell it to use our program (pair of shaders)
+    gl.useProgram(program);
+
+    // Turn on the position attribute
+    gl.enableVertexAttribArray(positionLocation);
+
+    // Bind the position buffer.
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    // Tell the position attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+    var size = 2; // 2 components per iteration
+    var type = gl.FLOAT; // the data is 32bit floats
+    var normalize = false; // don't normalize the data
+    var stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
+    var offset = 0; // start at the beginning of the buffer
+    gl.vertexAttribPointer(
+      positionLocation,
+      size,
+      type,
+      normalize,
+      stride,
+      offset
+    );
+
+    // Turn on the texcoord attribute
+    gl.enableVertexAttribArray(texcoordLocation);
+
+    // bind the texcoord buffer.
+    gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
+
+    // Tell the texcoord attribute how to get data out of texcoordBuffer (ARRAY_BUFFER)
+    var size = 2; // 2 components per iteration
+    var type = gl.FLOAT; // the data is 32bit floats
+    var normalize = false; // don't normalize the data
+    var stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
+    var offset = 0; // start at the beginning of the buffer
+    gl.vertexAttribPointer(
+      texcoordLocation,
+      size,
+      type,
+      normalize,
+      stride,
+      offset
+    );
+
+    // Set uniforms
+    setUniforms();
+
+    // Draw the rectangle.
+    var primitiveType = gl.TRIANGLES;
+    var offset = 0;
+    var count = 6;
+    gl.drawArrays(primitiveType, offset, count);
+
+    // Set uniforms
+    function setUniforms() {
+      gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height);
+      gl.uniform2f(textureSizeLocation, image.width, image.height);
+
+      // Edits
+      gl.uniform1f(exposureLocation, edits.exposure);
+    }
+
+    // Download image
+    if (capture) {
+      capture = false;
+
+      const dataURL = canvas.toDataURL("image/png");
+      const link = document.createElement("a");
+      link.download = "image.png";
+      link.href = dataURL;
+      link.click();
+    }
+  }
+
+  // Lookup uniforms
+  function lookupUniforms() {
+    resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+    textureSizeLocation = gl.getUniformLocation(program, "u_textureSize");
+
+    // Edits
+    exposureLocation = gl.getUniformLocation(program, "u_exposure");
+  }
+};
+
+const setRectangle = (gl, x, y, width, height) => {
+  var x1 = x;
+  var x2 = x + width;
+  var y1 = y;
+  var y2 = y + height;
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([x1, y1, x2, y1, x1, y2, x1, y2, x2, y1, x2, y2]),
+    gl.STATIC_DRAW
+  );
+};
+
+// Edit image
+function editImage(drawCanvas) {
+  $("#exposure").on("input", (e) => {
+    edits.exposure = e.target.value;
+    $("#exposure-value").text(e.target.value);
+    drawCanvas();
+  });
+}
